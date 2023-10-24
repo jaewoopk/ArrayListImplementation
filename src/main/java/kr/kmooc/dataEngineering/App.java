f5b64f9a17d103fadc9488173e8a724e4ad7b1b4
jaewoopk <jaewoopk000@naver.com>
2023-10-24 18:47:46 +0900

    Implementation MyLinkedList but not yet

--- src/main/java/kr/kmooc/dataEngineering/App.java
+++ src/main/java/kr/kmooc/dataEngineering/App.java
@@ -1,115 +1,149 @@
 package kr.kmooc.dataEngineering;
 
 import kr.kmooc.dataEngineering.homework1.MyArrayList;
+import kr.kmooc.dataEngineering.homework2_3.MyLinkedList;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Queue;
 import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("<Assignment 2-3>");
         List<Integer> arrayList = new ArrayList<Integer>();
         Random r = new Random();
-        for (int i = 0; i < 1000; i++)
+        for(int i = 0 ; i < 1000 ; i++)
             arrayList.add(r.nextInt(1000));
 
-        List<Integer> testArrayList = new ArrayList<Integer>();
-        for (Integer val : arrayList)
-            testArrayList.add(val);
+        LinkedList<Integer> testLinkedList = new LinkedList<Integer>();
+        for(Integer val : arrayList)
+            testLinkedList.add(val);
 
-        List<Integer> yourArrayList = new MyArrayList<Integer>();
-        for (int i = 0; i < 1000; i++)
-            yourArrayList.add(i, arrayList.get(i)); // add test
+        List<Integer> yourLinkedList = new MyLinkedList<Integer>();
+        for(int i = 0 ; i < 1000 ; i++) {
+            yourLinkedList.add(i,arrayList.get(i)); // add test
+        }
         boolean isPass = true;
 
-        System.out.println("size test: " + (testArrayList.size() == yourArrayList.size()));
-        System.out.println("isEmpty test: " + (testArrayList.size() == yourArrayList.size()));
-        
-        for (int i = 0; i < testArrayList.size(); i++) {
-            if (!testArrayList.get(i).equals(yourArrayList.get(i))) {
+        System.out.println("size test: " + (testLinkedList.size() == yourLinkedList.size()));
+        System.out.println("isEmpty test: " + (testLinkedList.size() == yourLinkedList.size()));
+
+        for(int i = 0 ; i < testLinkedList.size(); i++) {
+            if(!testLinkedList.get(i).equals(yourLinkedList.get(i))) {
                 isPass = false;
                 break;
             }
         }
-        System.out.println("get test : " + isPass);
+        System.out.println("get test : "+ isPass);
 
 
-        for (int i = 0; i < 100; i++) {
+        for(int i = 0 ; i < 100 ; i++) {
             Integer val = arrayList.get(r.nextInt(1000));
-            if (testArrayList.contains(val) != yourArrayList.contains(val)) {
+            if(testLinkedList.contains(val) != yourLinkedList.contains(val)){
                 isPass = false;
                 break;
             }
         }
         System.out.println("contains test: " + isPass);
-        isPass = true;
-        for (int i = 0; i < 100; i++) {
-            if (!testArrayList.remove(i).equals(yourArrayList.remove(i))) {
+        isPass =true;
+        for(int i = 0 ; i < 100 ; i++) {
+            if(!testLinkedList.remove(i).equals(yourLinkedList.remove(i))) {
                 isPass = false;
                 break;
             }
         }
-        System.out.println("remove test : " + isPass);
+        System.out.println("remove test : " +isPass);
 
 
         Integer value = arrayList.get(r.nextInt(1000));
-        if (testArrayList.indexOf(value) == yourArrayList.indexOf(value))
+        if(testLinkedList.indexOf(value) == yourLinkedList.indexOf(value))
             System.out.println("indexOf test : true");
         else
             System.out.println("indexOf test : false");
 
-        if (testArrayList.lastIndexOf(value) == yourArrayList.lastIndexOf(value))
+        if(testLinkedList.lastIndexOf(value) == yourLinkedList.lastIndexOf(value))
             System.out.println("lastIndexOf test : true");
         else
             System.out.println("lastIndexOf test : false");
 
 
-        for (int i = 0; i < testArrayList.size(); i++) {
+        for(int i =0 ; i< testLinkedList.size(); i++) {
             Integer v = r.nextInt(1000);
-            testArrayList.set(i, v);
-            yourArrayList.set(i, v);
+            testLinkedList.set(i, v);
+            yourLinkedList.set(i, v);
         }
-        Iterator<Integer> listIterator = testArrayList.iterator();
-        Iterator<Integer> yourIterator = yourArrayList.iterator();
-        while (listIterator.hasNext() && yourIterator.hasNext()) {
-            if (!listIterator.next().equals(yourIterator.next())) {
+
+        Iterator<Integer> listIterator = testLinkedList.iterator();
+        Iterator<Integer> yourIterator = yourLinkedList.iterator();
+        while(listIterator.hasNext() && yourIterator.hasNext()) {
+            if(!listIterator.next().equals(yourIterator.next())) {
                 isPass = false;
                 break;
             }
         }
-        System.out.println("set test: " + isPass);
+        System.out.println("set test : " + isPass);
         System.out.println("Iterator test: " + isPass);
 
-        ListIterator<Integer> listListIterator = testArrayList.listIterator(testArrayList.size());
-        ListIterator<Integer> yourListIterator = yourArrayList.listIterator(yourArrayList.size());
+        ListIterator<Integer> listListIterator = testLinkedList.listIterator(testLinkedList.size());
+        ListIterator<Integer> yourListIterator = yourLinkedList.listIterator(yourLinkedList.size());
         isPass = true;
-        while (listListIterator.hasPrevious() && yourListIterator.hasPrevious()) {
-            if (!listListIterator.previous().equals(yourListIterator.previous())) {
+        while(listListIterator.hasPrevious() && yourListIterator.hasPrevious()) {
+            if(!listListIterator.previous().equals(yourListIterator.previous())) {
                 isPass = false;
                 break;
             }
         }
         System.out.println("listIterator test: " + isPass);
-
-        Object[] testArray = testArrayList.toArray();
-        Object[] yourArray = yourArrayList.toArray();
+        Object[] testArray = testLinkedList.toArray();
+        Object[] yourArray = yourLinkedList.toArray();
         isPass = true;
-        for (int i = 0; i < testArray.length; i++)
-            if (!testArray[i].equals(yourArray[i])) {
+        for(int i = 0 ; i < testArray.length ; i++)
+            if(!testArray[i].equals(yourArray[i])) {
                 isPass = false;
                 break;
             }
         System.out.println("toArray test : " + isPass);
 
         isPass = true;
-        testArrayList.clear();
-        yourArrayList.clear();
+        testLinkedList.clear();
+        yourLinkedList.clear();
 
-        if (testArrayList.size() != yourArrayList.size())
+        Queue<Integer> testQueue = new LinkedList<>(testLinkedList);
+        Queue<Integer> yourQueue = new MyLinkedList<>(yourLinkedList);
+
+        if(testLinkedList.size() != yourLinkedList.size())
             isPass = false;
         System.out.println("clear test : " + isPass);
+
+        System.out.println("<Queue Test>");
+        Integer val = r.nextInt();
+        testQueue.offer(val);
+        yourQueue.offer(val);
+        val = r.nextInt();
+        testQueue.offer(val);
+        yourQueue.offer(val);
+
+        try {
+            System.out.println("remove test : "+ testQueue.remove().equals(yourQueue.remove()) );
+        } catch (Exception e) {
+            System.out.println("remove test : false");
+        }
+        System.out.println("poll test : " + testQueue.poll().equals(yourQueue.poll()));
+        try {
+            System.out.println("element test: "+ testQueue.element().equals(yourQueue.element()) );
+
+        }catch(Exception e){
+            System.out.println("element test: true");
+        }
+        if(testQueue.peek() == null && yourQueue.peek() == null)
+            System.out.println("peek test : true");
+        else
+            System.out.println("peek test : false");
+
 
     }
 }
---
+++ src/main/java/kr/kmooc/dataEngineering/homework2_3/MyLinkedList.java
@@ -0,0 +1,296 @@
+package kr.kmooc.dataEngineering.homework2_3;
+
+import java.util.AbstractSequentialList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Queue;
+
+public class MyLinkedList<E> implements List<E>, Queue<E> {
+    private MyNode<E> first;
+    private MyNode<E> last;
+    private int size;
+
+    public MyLinkedList() {
+        first = null;
+        last = null;
+        size = 0;
+    }
+
+    public MyLinkedList(Collection<? extends E> c) {
+        this();
+        for (E value : c) {
+            add(value);
+        }
+    }
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean offer(E e) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public E remove() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public E poll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public E element() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    @Override
+    public boolean contains(Object o) {
+        if (size == 0) {
+            return false;
+        }
+        MyNode<E> cursor = first;
+        do {
+            if (cursor.getItem().equals(o)) {
+                return true;
+            }
+            cursor = cursor.getNext();
+        } while(cursor != null);
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean add(E e) {
+        if (first == null) {
+            // 비어 있을 때
+            MyNode<E> newNode = new MyNode<>(null, e, null);
+            first = newNode;
+            last = newNode;
+            size++;
+        } else {
+            // 비어 있지 않을 때
+            MyNode<E> newNode = new MyNode<>(last, e, null);
+            last.setNext(newNode);
+            last = newNode;
+            size++;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        if (size == 0) {
+            return "[]";
+        }
+
+        String result = "[";
+        result += first.getItem();
+        MyNode<E> cursor = first.getNext();
+
+        while (cursor != null) {
+            result += ", " + cursor.getItem();
+            cursor = cursor.getNext();
+        }
+        result += "]";
+        return result;
+    }
+    @Override
+    public boolean remove(Object o) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+    }
+
+    private MyNode<E> getNode(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bounds for length " + size);
+        }
+
+        int cIdx = 0;
+        MyNode<E> cursor = first;
+
+        do {
+            if (cIdx == index) {
+                return cursor;
+            }
+            cursor = cursor.getNext();
+        } while(cursor != null);
+        return null;
+    }
+
+    @Override
+    public E get(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bounds for length " + size);
+        }
+
+        int cIdx = 0;
+        MyNode<E> cursor = first;
+
+        do {
+            if (cIdx == index) {
+                return cursor.getItem();
+            }
+            cursor = cursor.getNext();
+        } while(cursor != null);
+        return null;
+    }
+    @Override
+    public E set(int index, E element) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (size == index) {
+            add(element);
+            size--;
+        } else {
+            // 중간에 추가
+            MyNode<E> next = getNode(index);
+            MyNode<E> prev = next.getPrev();
+            // 새로운 노드 추가
+            MyNode<E> newNode = new MyNode<E>(prev, element, next);
+
+            // prev에 대한 참조 재조정
+            if (prev != null) {
+                // 처음 위치에 추가하는 것이 아님
+                prev.setNext(newNode);
+            } else {
+                first = newNode;
+            }
+            next.setPrev(newNode);
+        }
+        size++;
+    }
+
+    @Override
+    public E remove(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        int cIdx = 0;
+        MyNode<E> cursor = first;
+
+        do {
+            if (cursor.getItem().equals(o)) {
+                return cIdx;
+            }
+            cursor = cursor.getNext();
+            cIdx++;
+        } while(cursor != null);
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        int cIdx = size - 1;
+        MyNode<E> cursor = last;
+        do {
+            if (cursor.getItem().equals(o)) {
+                return cIdx;
+            }
+            cursor = cursor.getPrev();
+            cIdx--;
+        } while(cursor != null);
+        return -1;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        // TODO Auto-generated method stub
+
+        return null;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
